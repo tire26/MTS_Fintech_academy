e35aeba32b53112c38acee0eab54632a6ccf853b
Ivan <73220462+tire26@users.noreply.github.com>
2023-12-09 15:37:49 +0300

    выполнено первое дз

---
+++ HW-1/src/main/java/mts/ru/Main.java
@@ -0,0 +1,38 @@
+package mts.ru;
+
+import java.text.DecimalFormat;
+
+public class Main {
+
+    private static final DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+    public static void main(String[] args) {
+        ProductPurchaseInfo productPurchaseInfo1 = new ProductPurchaseInfo(
+                100,
+                121.75,
+                0.75
+        );
+        printPurchaseSums(productPurchaseInfo1);
+
+        ProductPurchaseInfo productPurchaseInfo2 = new ProductPurchaseInfo(
+                1215,
+                1212.25,
+                42.575
+        );
+        printPurchaseSums(productPurchaseInfo2);
+
+        ProductPurchaseInfo productPurchaseInfo3 = new ProductPurchaseInfo(
+                2,
+                150000.00,
+                59.1
+        );
+        printPurchaseSums(productPurchaseInfo3);
+
+
+    }
+
+    public static void printPurchaseSums(ProductPurchaseInfo productPurchaseInfo) {
+        System.out.println("Сумма без скидки: " + ProductPurchaseInfo.getPurchaseSumWithoutDiscount(productPurchaseInfo));
+        System.out.println("Сумма со скидкой: " + ProductPurchaseInfo.getPurchaseSumWithDiscount(productPurchaseInfo));
+    }
+}
