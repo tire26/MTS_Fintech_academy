4a972fd35ac26864b2c713ac146aef6637717b80
Ivan <73220462+tire26@users.noreply.github.com>
2023-12-19 22:22:57 +0300

    исправлено: некорректно вызывал дефолтный метод

--- src/main/java/mts/ru/Main.java
+++ src/main/java/mts/ru/Main.java
@@ -1,11 +1,31 @@
 package mts.ru;
 
+import mts.ru.pet.Parrot;
+import mts.ru.pet.Pet;
+import mts.ru.predator.Predator;
+import mts.ru.predator.Shark;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        CreateAnimalService createAnimalService = new CreateAnimalServiceImpl();
+
+        CreateAnimalService createAnimalService = new CreateAnimalService() {
+            @Override
+            public Pet generatePet() {
+                return new Parrot("кеша", new BigDecimal(200), "говорящий");
+            }
+
+            @Override
+            public Predator generatePredator() {
+                return new Shark("кусь", new BigDecimal(20000), "кусается", List.of());
+            }
+        };
         System.out.println("Генерация животных методом из CreateAnimalService");
         createAnimalService.createUniqueAnimals();
 
+        createAnimalService = new CreateAnimalServiceImpl();
         System.out.println("Генерация животных методом из CreateAnimalServiceImpl");
         CreateAnimalServiceImpl createAnimalServiceImpl = (CreateAnimalServiceImpl) createAnimalService;
         createAnimalServiceImpl.createUniqueAnimals();
