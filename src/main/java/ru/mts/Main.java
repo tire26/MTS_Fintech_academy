77e281d5e09ee6cf734611c4268aff2caa6e30aa
Ivan <73220462+tire26@users.noreply.github.com>
2024-02-11 15:56:58 +0300

    вызов методов animalsRepository по расписанию каждую минуту

--- src/main/java/ru/mts/Main.java
+++ src/main/java/ru/mts/Main.java
@@ -2,31 +2,12 @@ package ru.mts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.mts.animalsconstructstarter.model.Animal;
-import ru.mts.repository.AnimalsRepository;
-
-import java.time.format.DateTimeFormatter;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Main { 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
-        AnimalsRepository animalsRepository = applicationContext.getBean(AnimalsRepository.class);
-        animalsRepository.printDuplicate();
-        System.out.println("-----------------------------------");
-
-        String[] leapYearNames = animalsRepository.findLeapYearNames();
-        for (String leapYearName : leapYearNames) {
-            System.out.println("Животное с високосным годом рождения: " + leapYearName);
-        }
-        System.out.println("-----------------------------------");
-
-        int N = 15;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        Animal[] olderAnimal = animalsRepository.findOlderAnimal(N);
-        for (Animal animal : olderAnimal) {
-            System.out.println("Животное с возрастом больше " + N + " лет:" + animal.getName() + "| дата рождения: " + animal.getBirthDate().format(formatter));
-        }
+        SpringApplication.run(Main.class, args);
     }
 }
---
+++ src/main/java/ru/mts/ScheduledTask.java
@@ -0,0 +1,36 @@
+package ru.mts;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.mts.animalsconstructstarter.model.Animal;
+import ru.mts.repository.AnimalsRepository;
+
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class ScheduledTask {
+    private final AnimalsRepository animalsRepository;
+
+    public ScheduledTask(AnimalsRepository animalsRepository) {
+        this.animalsRepository = animalsRepository;
+    }
+
+    @Scheduled(fixedRate =60000)
+    public void task() {
+        animalsRepository.printDuplicate();
+        System.out.println("-----------------------------------");
+
+        String[] leapYearNames = animalsRepository.findLeapYearNames();
+        for (String leapYearName : leapYearNames) {
+            System.out.println("Животное с високосным годом рождения: " + leapYearName);
+        }
+        System.out.println("-----------------------------------");
+
+        int N = 15;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        Animal[] olderAnimal = animalsRepository.findOlderAnimal(N);
+        for (Animal animal : olderAnimal) {
+            System.out.println("Животное с возрастом больше " + N + " лет:" + animal.getName() + "| дата рождения: " + animal.getBirthDate().format(formatter));
+        }
+    }
+}
